1cf3398e0b87de847f154fd5b74bd3c8de90ae3c
vagrant <vagrant@localhost.localdomain>
2014-10-10 06:31:32 +0000

    init

---
+++ aaa/custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+ $('#js-create-user').click(function(e){
+  $('#js-diolog-content').dialog({
+      modal:true,
+open:function(){
+$('#js-new-user-sudmit'),click(function(e){
+var userume=$('#js-new-username'),val();
+alert(usename);
+});
+},
+        close: function(event,ui){
+	$('#js-create-user').html("closeし　　た　　ね");
+		}
+ 	});
+});
+});	
+});
---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+de^^^^su
+
+<?php 
+$date = date("Y-m-d H:i:s");
+print($date);
+?>de-su
+</body>
+</html>
